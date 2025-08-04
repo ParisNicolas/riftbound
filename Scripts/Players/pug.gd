bd7a1edb004f3f5d80220f4877be6465a8371cb5
parisnicolas <d47474935@alumnos.itr.edu.ar>
2025-08-04 19:31:12 -0300

    Multijugador

--- Scripts/Players/pug.gd
+++ Scripts/Players/pug.gd
@@ -5,20 +5,29 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+	
+func _ready():
+	if is_multiplayer_authority():
+		$Camera2D.make_current()
+
 func _physics_process(delta: float) -> void:
+	if !is_multiplayer_authority(): return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("mover_izquierda", "mover_derecha")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
